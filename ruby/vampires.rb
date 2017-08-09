ae85b7be73b1af1f5cfac78dbaafb41319e0dcb5
Shannon Nolan <shannonnolan17@gmail.com>
2017-08-09 14:14:12 -0400

    Add detailed vampire verifier

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,3 +1,8 @@
+puts "How many employees are you processing?"
+  user_input = gets.to_i
+
+while user_input > 0
+
 puts "What is your name?"
   name = gets.chomp
 
@@ -13,13 +18,27 @@ puts "Our company cafeteria serves garlic bread. Should we order some for you?"
 puts "Would you like to enroll in the company’s health insurance?"
   insurance = gets.chomp == "yes"
 
-if name == "Drake Cula" || "Tu Fang"
+if name == "Drake Cula" || name == "Tu Fang"
   puts "Definitely a vampire"
 elsif(age_check && garlic) || insurance
   puts "Probably not a vampire"
 elsif
   puts "Almost certainly a vampire"
-elsif (age_check == false || garlic == "no") || insurance == "no"
+elsif (age_check == false && garlic == "no") || insurance == "no"
   puts "Probaly not a vampire"
-
+elsif
+  puts "Results are inconclusive"
 end
+puts "Please list your allergies one by one. Type done when finished."
+allergies = gets.chomp
+until allergies == "done"
+      allergies = gets.chomp
+      if allergies == "sunshine"
+        puts "Probably a Vampire"
+        break
+      end
+end
+user_input -= 1
+end
+
+puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends!"
