2753f47a29c6505ae489672e6e3c9809c87a26bb
Shannon Nolan <shannonnolan17@gmail.com>
2017-08-10 13:08:40 -0400

    Add decrypt method

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,19 +1,30 @@
 #write encrypt method that advances every letter of string one letter forward
+#define word to be encrypted
 test_word = "cat"
-#seperate each letter
-#advance letter
-#put back together
-index = 0
-while index < test_word.length
-  test_word[index] =
-    test_word[index].next!
-  index += 1
+
+#define counting variable
+counter = 0
+#write loop to advance each letter forward and redefine encrypted word
+while counter < test_word.length
+  test_word[counter] =
+    test_word[counter].next!
+  counter += 1
 end
+#print redefined excrypted word
 puts test_word
 
 
 #write decrypt method
 #define alphabet
+alphabet = "abcdefghijklmnopqrstuvwxyz"
 #find letter index for each position of string and subtract 1
+counter = 0
+alphabet.index(test_word[1])
+while counter < test_word.length
+  alphabet.index(test_word[counter])
+
+  counter += 1
+end
+
 #find alphabet letter by using those indices
 #add string of alphabet letters to put back together
