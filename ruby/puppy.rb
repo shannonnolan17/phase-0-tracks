02f3f2987a0a42566b77dab9c206d04a5c13eda1
Shannon Nolan <shannonnolan17@gmail.com>
2017-08-20 12:18:28 -0400

    Add characteristics and behaviors to puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,21 @@
+
+#BREED --------------------------
+#
+
+#CHARACTERISTICS -------------------
+#size currently:
+#size when full grown:
+#Friendly:
+#Name:
+#Color(s):
+#Playfulness:
+
+#BEHAVIOR -------------------
+#Bark
+#Jump
+#Lick
+#Be cute
+#Sit
+#Lay
+#Stay
+
