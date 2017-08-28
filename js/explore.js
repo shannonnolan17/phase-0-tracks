f5e23cdc6c10cba11302ebbbdc9da76fb234efae
Shannon Nolan <shannonnolan17@gmail.com>
2017-08-28 10:08:01 -0400

    Update string reverser

--- js/explore.js
+++ js/explore.js
@@ -1,6 +1,16 @@
 // declare function with parameter
-// for each letter place in reverse place
+// declare new empty string to return
+// for each letter less than the length of the string
+//start at end of string and add last letter to new string
+// continue to add letters to new string in reverse order
+// return the new string
 
-function reverseString(string){
-  for (var )
+function reverseString(str){
+  var newString = "";
+  for (var i = str.length - 1; i >= 0; i--) {
+    newString += str[i];
+    }
+  return newString;
 }
+
+console.log(reverseString("hello"));
