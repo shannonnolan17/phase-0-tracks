90ffe293ccf14deee702f6a6f1474b4182e28539
Shannon Nolan <shannonnolan17@gmail.com>
2017-08-29 16:18:36 -0400

    Add value matcher

--- js/algos.js
+++ js/algos.js
@@ -15,5 +15,52 @@ function longestWord(arr){
   return longest
   }
 }
+
+//define function with two parameters
+//create empty arrays
+//iterate through each hash and place the values into empty
+//arrays
+//compare each index of the array and compare with index of o
+//other array to look for matches
+//if there are matches return true
+
+function matchingPairs(obj1, obj2){
+  arrayPair1 = []
+  arrayPair2 = []
+  for (var value in obj1){
+    arrayPair1.push(obj1[value]);
+}
+  for (var value in obj2){
+    arrayPair2.push(obj2[value]);
+}
+
+  for (var i = 0; i < arrayPair1.length; i++){
+    if (arrayPair2.indexOf(arrayPair1[i]) > -1){
+      return true;
+    }else {
+      return false;
+  }
+}
+
+  // for (var i = 0; i < arrayPair1.length; i++) {
+  //   for (var e = 0; e < arrayPair2.length; e++) {
+  //       if (arrayPair1[i] == arrayPair2[e]){
+  //         return true;
+  //       }else{
+  //         return false;
+  //       }
+  //       }
+  //   }
+
+}
+
+
+console.log(matchingPairs({name: "Steven", age: 54}, {name: "Tamir", age: 54}));
+console.log(matchingPairs({animal: "Dog", legs: 4}, {animal: "Dog", legs: 3}));
+console.log(matchingPairs({animal: "Cat", legs: 4}, {animal: "Dog", legs: 3}));
+
+
 console.log(longestWord(["long phrase","longest phrase","longer phrase"]));
 console.log(longestWord(["Emma", "Shannon"]));
+
+
