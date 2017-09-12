499cf3bb671b53f7ef78df3d1eb363ee186ac42a
Shannon Nolan <shannonnolan17@gmail.com>
2017-09-12 10:39:21 -0400

    Add new routes

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -1,9 +1,9 @@
 # require gems
 require 'sinatra'
-require 'sqlite3'
+# require 'sqlite3'
 
-db = SQLite3::Database.new("students.db")
-db.results_as_hash = true
+# db = SQLite3::Database.new("students.db")
+# db.results_as_hash = true
 
 # write a basic GET route
 # add a query parameter
@@ -19,28 +19,49 @@ get '/about/:person' do
   "#{person} is a programmer, and #{person} is learning Sinatra."
 end
 
+get '/contact' do
+  "123 Main Street Philadelpia, PA"
+end
+
 get '/:person_1/loves/:person_2' do
   "#{params[:person_1]} loves #{params[:person_2]}"
 end
 
+get '/great_job/:person_name' do
+  person_name = params[:person_name]
+  if person_name
+    "Good job, #{persons_name}!"
+  else
+    "Good job!"
+  end
+end
+
+get '/adder/:number_1/:number_2' do
+  number_1 = params[:number_1]
+  number_2 = params[:number_2]
+  answer = number_1.to_i + number_2.to_i
+  return answer.to_s
+end
+
+
 # write a GET route that retrieves
 # all student data
-get '/students' do
-  students = db.execute("SELECT * FROM students")
-  response = ""
-  students.each do |student|
-    response << "ID: #{student['id']}<br>"
-    response << "Name: #{student['name']}<br>"
-    response << "Age: #{student['age']}<br>"
-    response << "Campus: #{student['campus']}<br><br>"
-  end
-  response
-end
+# get '/students' do
+#   students = db.execute("SELECT * FROM students")
+#   response = ""
+#   students.each do |student|
+#     response << "ID: #{student['id']}<br>"
+#     response << "Name: #{student['name']}<br>"
+#     response << "Age: #{student['age']}<br>"
+#     response << "Campus: #{student['campus']}<br><br>"
+#   end
+#   response
+# end
 
-# write a GET route that retrieves
-# a particular student
+# # write a GET route that retrieves
+# # a particular student
 
-get '/students/:id' do
-  student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
-  student.to_s
-end
+# get '/students/:id' do
+#   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
+#   student.to_s
+# end
